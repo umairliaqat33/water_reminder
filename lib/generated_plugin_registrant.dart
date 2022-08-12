fa67c06e46c8a0c9ec603ed9bbc517305aa0a7fe
Umair <umairliaqat552@gmail.com>
2022-08-12 15:14:56 +0500

    s1

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -10,9 +10,10 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_database_web/firebase_database_web.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
